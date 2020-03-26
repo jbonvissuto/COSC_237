b9c5e699889f001130d43c47bd7934dbd5a6beca
Batman <jackakabatman@gmail.com>
2020-03-26 19:42:12 -0400

    Completed Circle.Java and its driver class assingment3_2driver.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,27 +1,64 @@
 //COSC 237-101
 //Name: Jackson K. Bonvissuto
 //ID: 0716190
-public class Circle {
+public class Circle extends Point {
+  protected double radius;
   
+  public Circle() {
+	  super();
+	  radius =1;
+  }
   
+  public Circle(double xValue, double yValue, double radValue) {
+	  super(xValue,yValue);
+	  radius = radValue;
+  }
   
+ public void setCircle(double xValue, double yValue, double radValue) {
+	 setPoint(xValue, yValue);
+	 setRadius(radValue);
+ }
   
+  public void setRadius(double radValue){
+	  radius = radValue;
+  }
   
+  public double getRadius() {
+	  return radius;
+  }
   
+  public double getArea() {
+	return(Math.PI * radius *radius);
+  }
   
+  public double getCircumference() {
+	  return(Math.PI * radius * 2);
+  }
   
+  public String toString() {
+	  return "( " + super.toString()  +" radius: " +  String.format("%.2f", radius) + " )";
+  }
   
+  public boolean equals(Circle otherCircle) {
+	 return( super.equals(otherCircle) && radius == otherCircle.radius) ;
+  }
   
+  public void makeCopy(Circle otherCircle) {
+	  super.makeCopy(otherCircle);
+	  radius = otherCircle.radius;
+  }
   
+  public Circle getCopy() {
+	  Circle temp = new Circle();
+	  super.getCopy();
+	  temp.radius = radius;
+	  return temp;
+  }
   
+  public void printCircle() {
+	  System.out.print(toString());
+  }
   
-  
-  
-  
-  
-  
-  
-  
-  
+ 
   
 }
--- src/assingment3_2driver.java
+++ src/assingment3_2driver.java
@@ -6,5 +6,37 @@ public class assingment3_2driver{
 //Test
 	public static void main(String[]args) {
 	
+	Circle circle1 = new Circle();	
+	Circle circle2 = new Circle(1,1,3);
+	System.out.print("circle1: ");
+	circle1.printCircle();
+	System.out.println();
+	System.out.print("circle2: ");
+	circle2.printCircle();
+	System.out.println();
+	circle1.setRadius(10);
+	Circle circle3 = new Circle();
+	circle3 = circle1.getCopy();
+	System.out.print("circle1: ");
+	circle1.printCircle();
+	System.out.println();
+	System.out.print("circle3: ");
+	circle3.printCircle();
+	System.out.println();
+	if(circle1.equals(circle3))
+		System.out.println("circle1 equals circle3");
+	else
+		System.out.println("circle1 doesn't equal circle3");
+	circle3.setPoint(-9, -9);
+	System.out.print("circle3: ");
+	circle3.printCircle();
+	System.out.println();
+	if(circle1.equals(circle3))
+		System.out.println("circle1 equals circle3");
+	else
+		System.out.println("circle1 doesn't equal circle3");
+	System.out.println("circle1's area is: " + circle1.getArea() + " and it's circumfrence is: " + circle1.getCircumference());
+	System.out.println("circle2's area is: " + circle2.getArea() + " and it's circumfrence is: " + circle2.getCircumference());
+	System.out.println("circle3's area is: " + circle3.getArea() + " and it's circumfrence is: " + circle3.getCircumference());
 	}
   }
