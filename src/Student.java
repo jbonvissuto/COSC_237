2c9b3dd72dc0ff069131e6e7a2dff038588c7926
Batman <jackakabatman@gmail.com>
2020-04-13 01:50:40 -0400

    added constructors to Student

--- src/Student.java
+++ src/Student.java
@@ -5,10 +5,25 @@ public class Student extends Person
 {
   // Fields
   //    -- Your Code Here
+  private int studentId;
+  private boolean tuitionPaid;
+  private int numOfClass;
   
   // Constructors
   //    -- Your Code Here
+  public Student(){
+	  super();
+	  studentId = 0;
+	  tuitionPaid = false;
+	  numOfClass = 0;
+  }
   
+  public Student(String first, String last, int id, boolean paid, int numClass) {
+	  super(first, last);
+	  studentId = id;
+	  tuitionPaid = paid;
+	  numOfClass = numClass;
+  }
   // Methods
   //    -- Your Code Here
 }
